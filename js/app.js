65ac5ccf7b289bdb46099c54c1bde9f9e43bf474
MollyGreace <greace.ss@gmail.com>
2017-12-15 11:11:18 -0500

    añade modal login

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 // La funcionalidad de tu proyecto
+$('#modalLogin').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
+
+$('#modalRegister').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
